331458ae637dd0a774861d4dac3ae57b1919c5cd
shahabas <shezilaman@gmail.com>
2023-12-19 17:43:44 +0530

    new

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,48 @@
 import React, { Component } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
-// you can explore more - and check as how to use materiul ui
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-
-// implement the class below
 class Carousel extends Component {
-  
+    constructor(){
+        super()
+        this.state={
+            ind:0
+        }
+        this.next = this.next.bind(this)
+    }
+    
+    next(){
+        console.log("here",this)
+        this.setState(()=>{
+            return{
+                ind: (this.state.ind!=images.length-1)?(this.state.ind + 1):(0)
+            }
+        })
+    }
+    previous(){
+        console.log("here",this)
+        this.setState(()=>{
+            return{
+                ind: (this.state.ind!=images.length-1)?(this.state.ind - 1):(0)
+            }
+        })
+    }
+    render(){
+        return(
+            <>
+            <div className="body">
+            <div className="container">
+            <ArrowBackIosIcon className="leftarr" fontSize='large' onClick={this.previous}/>
+            <h1  className="title-overlay ">{images[this.state.ind].title}</h1>
+            <h1  className="subtitle-overlay ">{images[this.state.ind].subtitle}</h1>
+            <div><img src={images[this.state.ind].img}/></div>
+            <ArrowForwardIosIcon className="rightarr" fontSize='large' onClick={this.next}/>
+            </div>
+            </div>
+            </>
+        )
+    }
+    
 }
-
 export default Carousel;
